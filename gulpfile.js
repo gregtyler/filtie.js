3302e5b18962909d3c8fcef1fc58142c3b70ffa7
Greg Tyler <greg@gregtyler.co.uk>
2016-03-13 09:58:53 +0000

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+// Load modules
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+var rename = require('gulp-rename');
+var replace = require('gulp-replace');
+var sourcemaps  = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+
+// Load data from package.json
+var pkg = require('./package.json');
+
+// The default build task
+gulp.task('build', function() {
+  return gulp.src('src/filtie.js')
+    .pipe(sourcemaps.init())
+    .pipe(eslint()) //JSCS
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(replace('<<VERSION>>', pkg.version))
+    .pipe(gulp.dest('build')) // Output file
+    .pipe(uglify({outSourceMap: true, preserveComments: 'license'})) // Uglify
+    .pipe(rename({extname: '.min.js'})) // Rename
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('build')) // Output minified
+  ;
+});
+
+gulp.task('watch', function() {
+  return gulp.watch('src/filtie.js', ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
